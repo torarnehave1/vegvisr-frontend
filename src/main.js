e5515ce4ec6e7ad542701e54255d1863c6fd26a6
Tor Arne Håve <torarnehave@gmail.com>
2025-03-10 15:12:46 +0100

    Remove test router and associated view from the application

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import testrouter from './router/testrouter'
 
 const app = createApp(App)
 
 app.use(router)
-app.use('/test', testrouter)
 
 app.mount('#app')
