7965f39d07d71c57ebd03a37938ee80bb46a0efb
Tor Arne Håve <torarnehave@gmail.com>
2025-03-11 09:40:56 +0100

    Rename worker to vegvisr-frontend and update middleware loader

--- worker/.wrangler/tmp/dev-vU5LMD/index.js
+++ worker/.wrangler/tmp/dev-vU5LMD/index.js
@@ -53,7 +53,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-1QkWQe/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-GNiWLa/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -85,7 +85,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-1QkWQe/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-GNiWLa/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
