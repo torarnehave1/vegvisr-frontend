b4fe08781dd9bcc89760fb32b983b56ccf552201
Tor Arne Håve <torarnehave@gmail.com>
2025-03-11 08:03:55 +0100

    Update import path for OpenAIView in openaiRouter

--- src/router/openaiRouter.js
+++ src/router/openaiRouter.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import OpenAIView from '../views/OpenAIView.vue'
+import OpenAIView from '@/views/OpenAIView.vue' // Updated import path
 
 const openaiRouter = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
