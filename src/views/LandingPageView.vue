7b073fc69dd96ff3810dfa1105650bf9b55a9e4f
Tor Arne Håve <torarnehave@gmail.com>
2025-03-20 09:42:54 +0100

    Update LandingPageView with new title and descriptive content

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>Another View</h1>
+    <h1>Phonetics, Energy & Ritual in the Vegvisr Framework</h1>
+    <p>
+      Explore the vibrational essence of sacred phonetics and how sound influences energy and ritual
+      practices.
+    </p>
+    <p>
+      By registering, you join a community dedicated to exploring the deeper connections between
+      sound, energy, and sacred practices. Vegvisr.org values your insights and presence as we
+      co-create an evolving framework of wisdom and ritual.
+    </p>
     <UserRegistration />
   </div>
 </template>
