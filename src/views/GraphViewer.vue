80c9bd5f53c6aa261c400860198ee3472ef9e9a9
Tor Arne Håve <torarnehave@gmail.com>
2025-04-21 22:19:32 +0200

    Fix API URL in GraphViewer.vue to correct endpoint format

--- src/views/GraphViewer.vue
+++ src/views/GraphViewer.vue
@@ -35,7 +35,7 @@
 import { ref, onMounted } from 'vue'
 
 const graphId = 'example-graph-id' // Replace with dynamic ID if needed
-const apiUrl = `https://knowledge.vegvisr.org/getknowgraph/getknowgraph?id=${graphId}`
+const apiUrl = `https://knowledge.vegvisr.org/getknowgraph?id=${graphId}`
 
 const graphData = ref({ nodes: [], edges: [] })
 const loading = ref(true)
