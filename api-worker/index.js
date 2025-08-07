19692bb99de28a7b4f2402f1aa4b812b05ffeb0a
Tor Arne Håve <torarnehave@gmail.com>
2025-08-07 08:45:26 +0200

    feat: Add alivenesslab.org and universi.no to domain mappings in API worker and branding modal

--- api-worker/index.js
+++ api-worker/index.js
@@ -23,6 +23,7 @@ const DOMAIN_ZONE_MAPPING = {
   'slowyou.training': '1417691852abd0e8220f60184b7f4eca', // vegvisr.org zone ID
   'movemetime.com': 'abb39e8d56446afe3ac098abd5c21732', // movemetime.com zone ID
   'universi.no': '3adef6348a9c36282014fd78d88d49f5', // universi.no zone ID
+  'alivenesslab.org': '4dc34fae60abef723cb8ae9ace5475f0', // alivenesslab.org zone ID
 }
 
 // Protected subdomains configuration - SECURITY CRITICAL
@@ -48,6 +49,7 @@ const PROTECTED_SUBDOMAINS = {
   'slowyou.training': ['www', 'api', 'mail', 'admin', 'blog', 'cdn', 'static'],
   'movemetime.com': ['www', 'api', 'mail', 'admin', 'blog', 'cdn', 'static'],
   'universi.no': ['www', 'api', 'mail', 'admin', 'blog', 'cdn', 'static'],
+  'alivenesslab.org': ['www', 'api', 'mail', 'admin', 'blog', 'cdn', 'static'],
 }
 
 // Security validation function for protected subdomains
